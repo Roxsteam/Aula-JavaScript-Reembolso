47f9d7f5050825013a3d543914ffd6c0404c5d61
Roxsteam <neves.aleex@gmail.com>
2025-12-18 21:52:06 -0300

    Criando objeto para pegar as informações do formulário

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 // seleciona os elementos do formulário. 
 
-
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 
 // pode usar o amount.oninput (mesmo função do addEventListener)
@@ -28,7 +30,34 @@ currency: "BRL",
 return value
     }
 
-  
+// captura do evento de submit do formulário para obter os valores
+
+form.onsubmit = (event) => {
+    // preventDefault() serve para prevenir o comportamento padrão de recarregar a página
+        event.preventDefault()
+
+  // cria um objeto com os detalhes da nova despesa.
+    const newExpense = {
+  // retorna os milissegundos desde a data de referência do JavaScript
+  // estratégia somente para ter um ID.      
+        id: new Date().getTime(),
+        expense: expense.value,
+        // retorna o value dentro do category
+        category_id: category.value,
+        //pega o texto selecionado dentro das opções do category
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
+    
+   
+    
+}
+
+
+
+
+ 
 
 
 
