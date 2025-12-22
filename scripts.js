4c41cfdfa90166d48e6130ee2c683a4c29a2a4be
Alice Neves <neves.aleex@gmail.com>
2025-12-22 18:07:02 -0300

    Adicionando o valor total

--- scripts.js
+++ scripts.js
@@ -7,6 +7,10 @@ const category = document.getElementById("category")
 
 // seleciona os elementos da lista
 const expenseList = document.querySelector("ul")
+const expenseTotal = document.querySelector("aside header h2")
+
+// pode-se usar o querySelector para navegar por dentro dos elementos, chegando no desejado
+const expensesQuantity = document.querySelector("aside header p span")
 
 
 // pode usar o amount.oninput (mesmo função do addEventListener)
@@ -22,9 +26,8 @@ amount.oninput = () => {
     // aualiza o valor do input
        amount.value = formatCurrencyBRL(value)
     }
-
-    function formatCurrencyBRL(value) {
 // formato o valor no padrão BRL (Real Brasileiro)
+function formatCurrencyBRL(value) {
 value = value.toLocaleString("pt-BR", {
 style: "currency",
 currency: "BRL",
@@ -34,7 +37,6 @@ return value
     }
 
 // captura do evento de submit do formulário para obter os valores
-
 form.onsubmit = (event) => {
     // preventDefault() serve para prevenir o comportamento padrão de recarregar a página
         event.preventDefault()
@@ -85,18 +87,26 @@ function expenseAdd (newExpense) {
 // adiciona o nome e categoria na div das informações da despesa
         expenseInfo.append(expenseName, expenseCategory)
         
+ // cria um elemento span
+        const expenseAmount = document.createElement("span")
+        expenseAmount.classList.add("expense-amount") 
+        expenseAmount.innerHTML = `<small>R$</small> ${newExpense.amount.toUpperCase().replace("R$", "")}`
         
-
-
+// criando o ícone de remover
+        const expenseRemove = document.createElement("img")
+        expenseRemove.classList.add("remove-icon")
+        expenseRemove.setAttribute("src", `img/remove.svg`)
+        expenseRemove.setAttribute("alt", "remover")
 
 
 // adiciona as informações no item
-expenseItem.append(expenseIcon, expenseInfo)
+expenseItem.append(expenseIcon, expenseInfo, expenseAmount, expenseRemove)
 
 // adiciona o item na lista
 expenseList.append(expenseItem)
 
-
+// executa a função para atualizar os valores totais após clicar em adicionar novo item na lista
+updateTotais()
 
     } catch (error) {
         
@@ -105,7 +115,62 @@ expenseList.append(expenseItem)
     }
 }
 
+// Atualiza os totais
+function updateTotais() {
 
+try {
+// recupera todos os itens (li) da lista (ul)
+const items = expenseList.children
+
+// usando o if ternário foi feita uma verificação para mudança da palavra "despesa" conforme a quantidade
+// leia-se se a quantidade de itens do items for maior que 01, a palavra será despesas, caso contrário, será despesa
+expensesQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas" : "despesa"}`
+
+// variável para incrementar o total
+let total = 0
+
+// percorrendo cada item (li) da lista (ul)
+for (let item = 0; item < items.length; item++){
+    const itemAmount = items[item].querySelector(".expense-amount")
+
+// remove caracteres não numéricos e substitui a vírgula pelo ponto 
+    let value = itemAmount.textContent.replace(/[^\d,]/g, "").replace(",",".")
+
+
+// converte o valor para float
+value = parseFloat(value)
+// Converter um número para float (ponto flutuante)
+// significa transformá-lo em um tipo de dado que pode ter casas decimais
+
+// verificar se é um número válido
+if (isNaN(value)) {
+return alert("Não foi possível calcular o total. O valor não parece ser um número")
+}
+
+// Incrementar o valor total (pode ser escrito: total = Number(total + value))
+total += Number(value)
+}
+
+
+// criando a small para adicionar o R$ formatado
+const symbolBRL = document.createElement("small")
+symbolBRL.textContent = "R$"
+
+// formato o valor e retira o R$
+total = formatCurrencyBRL(total).toUpperCase().replace("R$", "")
+
+// limpa o conteúdo do elemento
+expenseTotal.innerHTML = ""
+
+// adiciona o R$ e o valor total formatados
+expenseTotal.append(symbolBRL, total)
+
+
+} catch (error) {
+    console.log(error)
+    alert ("Não foi possível atualizar os valores totais")
+}
+}
 
 
 
